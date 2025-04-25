e62f843db3a929f9c239bc9c49b0aa9520b6617b
sifat789 <shikdersifat789@gmail.com>
2025-04-25 15:24:49 +0600

    os lab first push

---
+++ oslabsim/src/main/java/com/oslab/oslabsim/OslabsimApplication.java
@@ -0,0 +1,13 @@
+package com.oslab.oslabsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OslabsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OslabsimApplication.class, args);
+	}
+
+}
---
+++ oslabsim/src/test/java/com/oslab/oslabsim/OslabsimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oslab.oslabsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OslabsimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
